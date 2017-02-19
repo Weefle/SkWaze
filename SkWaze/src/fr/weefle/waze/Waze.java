8de57aff546f75e55fceee0143f1ce3edc5ea4df
Weefle <Weefle@DESKTOP-U9H07O1>
2017-02-19 22:37:07 +0100

    + Expression: Ping

--- SkWaze/src/fr/weefle/waze/Waze.java
+++ SkWaze/src/fr/weefle/waze/Waze.java
@@ -3,6 +3,7 @@ package fr.weefle.waze;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.lang.ExpressionType;
 
 public class Waze extends JavaPlugin {
 	
@@ -11,6 +12,7 @@ public class Waze extends JavaPlugin {
 		// TODO Auto-generated method stub
 		Skript.registerAddon(this);
 		Skript.registerEffect(WazeEffectTitle.class, "[waze] (send|create) title %string% with [subtitle] %string% to %player% for %integer% tick[s]");
+		Skript.registerExpression(WazeExpressionPing.class, Integer.class, ExpressionType.PROPERTY, "[waze] %player%['s] ping", "[waze] ping of %player%");
 		super.onEnable();
 	}
 
