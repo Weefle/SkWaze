a4825eaa3b0347d0864bec385c2388fe9248defd
BuildTools <unconfigured@null.spigotmc.org>
2017-03-01 20:42:04 +0100

    New recipe additions! (v0.3) + Recipe Effect added! + Recipe Clear Effect added!

--- SkWaze/src/fr/weefle/waze/Waze.java
+++ SkWaze/src/fr/weefle/waze/Waze.java
@@ -11,9 +11,11 @@ public class Waze extends JavaPlugin {
 	public void onEnable() {
 		// TODO Auto-generated method stub
 		Skript.registerAddon(this);
-		Skript.registerEffect(WazeEffectTitle.class, "[waze] (send|create) title %string% with [subtitle] %string% to %player% for %integer% tick[s]");
-		Skript.registerEffect(WazeEffectActionBar.class, "[waze] (send|create) action[bar] %string% to %player%");
+		Skript.registerEffect(WazeEffectTitle.class, "[waze] (send|create) title %string% with [sub[title]] %string% (to|for) %player% (for|to) %integer% tick[s]");
+		Skript.registerEffect(WazeEffectActionBar.class, "[waze] (send|create) action[bar] %string% (to|for) %player%");
 		Skript.registerExpression(WazeExpressionPing.class, Integer.class, ExpressionType.PROPERTY, "[waze] %player%['s] ping", "[waze] ping of %player%");
+		Skript.registerEffect(WazeEffectRecipe.class, "[waze] (create|register) [new] recipe[s] [for] %itemtype% with %itemtype%, %itemtype%, %itemtype%, %itemtype%, %itemtype%, %itemtype%, %itemtype%, %itemtype%, %itemtype%");
+		Skript.registerEffect(WazeEffectClearRecipe.class, "[waze] (remove|clear) [craft[ing]] recipe[s] (for|to) %itemtypes%");
 		super.onEnable();
 	}
 
---
+++ SkWaze/src/fr/weefle/waze/WazeEffectClearRecipe.java
@@ -0,0 +1,47 @@
+package fr.weefle.waze;
+
+import java.lang.reflect.InvocationTargetException;
+import org.bukkit.event.Event;
+import ch.njol.skript.aliases.ItemType;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+
+public class WazeEffectClearRecipe
+  extends Effect
+{
+  private Expression<ItemType> items;
+  
+  protected void execute(Event event)
+  {
+    ItemType[] arrayOfItemType;
+    int j = (arrayOfItemType = (ItemType[])this.items.getAll(event)).length;
+    for (int i = 0; i < j; i++)
+    {
+      ItemType type = arrayOfItemType[i];
+      try
+      {
+        Object craftingInstance = Reflection.nmsClass("CraftingManager").getMethod("getInstance", new Class[0]).invoke(null, new Object[0]);
+        Object recipes = Reflection.getField(craftingInstance.getClass(), "recipes").get(craftingInstance);
+        Reflection.getMethod(recipes.getClass(), "remove", new Class[0]).invoke(recipes, new Object[] { Integer.valueOf(type.getRandom().getType().getMaxStackSize()) });
+      }
+      catch (IllegalAccessException|NoSuchMethodException|InvocationTargetException e)
+      {
+        e.printStackTrace();
+      }
+    }
+  }
+  
+  public String toString(Event event, boolean b)
+  {
+    return "MFG recipes";
+  }
+  
+  @SuppressWarnings("unchecked")
+public boolean init(Expression<?>[] expressions, int i, Kleenean kleenean, ParseResult parseResult)
+  {
+    items = (Expression<ItemType>) expressions[0];
+    return true;
+  }
+}
