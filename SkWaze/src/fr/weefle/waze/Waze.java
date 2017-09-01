d9d629e6b57c5739dd485f62f5185ea35bb48f4a
Weefle <mr.weefle@gmail.com>
2017-09-01 20:09:28 +0200

    Testing JumpEvent

--- SkWaze/src/fr/weefle/waze/Waze.java
+++ SkWaze/src/fr/weefle/waze/Waze.java
@@ -1,14 +1,11 @@
 package fr.weefle.waze;
 
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import ch.njol.skript.Skript;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.util.SimpleEvent;
-import ch.njol.skript.registrations.EventValues;
-import ch.njol.skript.util.Getter;
-import fr.weefle.events.EvtJump;
+import fr.weefle.events.PlayerJumpEvent;
 import fr.weefle.waze.effects.WazeEffectActionBar;
 import fr.weefle.waze.effects.WazeEffectBossBar;
 import fr.weefle.waze.effects.WazeEffectClearRecipes;
@@ -23,6 +20,7 @@ public class Waze extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		instance = this;
+		PlayerJumpEvent.register(this);
 		Skript.registerAddon(this);
 		Skript.registerEffect(WazeEffectTitle.class, "[waze] (send|create) title %string% with [sub[title]] %string% (to|for) %player% (for|to) %integer% tick[s]");
 		Skript.registerEffect(WazeEffectActionBar.class, "[waze] (send|create) action[bar] %string% (to|for) %player%");
@@ -31,12 +29,12 @@ public class Waze extends JavaPlugin {
 		Skript.registerEffect(WazeEffectRecipe.class, "[waze] (create|register) [new] recipe[s] [for] %itemtype% with %itemtype%, %itemtype%, %itemtype%, %itemtype%, %itemtype%, %itemtype%, %itemtype%, %itemtype%, %itemtype%");
 		Skript.registerEffect(WazeEffectClearRecipes.class, "[waze] (remove|clear) [all] [craft[ing]] recipe[s]");
 		Skript.registerEffect(WazeEffectBossBar.class, "[waze] (create|send) [boss]bar %string% (with|at) %float% percent[s] (to|for) %player%");
-		Skript.registerEvent("Jump Event", SimpleEvent.class, EvtJump.class, "[waze] jump");
-		EventValues.registerEventValue(EvtJump.class, Player.class, new Getter<Player, EvtJump>() {
-		    public Player get(EvtJump e) {
-		        return e.getPlayer();
-		    }
-		}, 0);
+		Skript.registerEvent("Jump Event", SimpleEvent.class, PlayerJumpEvent.class, "[waze] jump[ing]");
+		/*EventValues.registerEventValue(PlayerJumpEvent.class, Player.class, new Getter<Player, PlayerJumpEvent>() {
+            public Player get(PlayerJumpEvent event) {
+                return event.getPlayer();
+            }
+        }, 0);*/
 }
 
 	public static Plugin getInstance() {
