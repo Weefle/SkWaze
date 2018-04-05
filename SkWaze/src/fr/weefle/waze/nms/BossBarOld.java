0a2ced8a07a91aa3a13d3e04464f897b21a03133
Weefle <mr.weefle@gmail.com>
2018-04-05 22:16:46 +0200

    Testing BossBarAPI

--- SkWaze/src/fr/weefle/waze/nms/BossBarOld.java
+++ SkWaze/src/fr/weefle/waze/nms/BossBarOld.java
@@ -1,31 +1,40 @@
 package fr.weefle.waze.nms;
 
 import org.bukkit.entity.Player;
-import me.confuser.barapi.BarAPI;
+import org.inventivetalent.bossbar.BossBarAPI.Color;
+import org.inventivetalent.bossbar.BossBarTimer;
+import org.inventivetalent.bossbar.PacketBossBar;
 
 public class BossBarOld implements BossBarAPI {
-
-	@SuppressWarnings("deprecation")
+	
+	private PacketBossBar pb = null;
 	@Override
 	public void sendBossBar(Player p, String message, double percent, String color, String id) {
 		
-			BarAPI.setMessage(p, message, (float) percent * 100);
+			//BarAPI.setMessage(p, message, (float) percent * 100);
+		pb.setMessage(message);
+        pb.setProgress((float) percent * 100);
+        pb.setColor(Color.valueOf(color));
+        pb.addPlayer(p);
 			
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public void removeBossBar(Player p, String id) {
 		
-			BarAPI.removeBar(p);
+			//BarAPI.removeBar(p);
+			pb.removePlayer(p);
 		
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public void sendBossBarTimer(Player p, String message, double percent, String color, int time, String id) {
 		
-            BarAPI.setMessage(p, message, time / 20);
+            //BarAPI.setMessage(p, message, time / 20);
+            new BossBarTimer(pb, (float) percent * 100, time / 20);
+            pb.setMessage(message);
+            pb.setColor(Color.valueOf(color));
+            pb.addPlayer(p);
           
         }
 	
