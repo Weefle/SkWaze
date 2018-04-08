2c5420611bec906d1e93964f9962eab158a19eae
Weefle <mr.weefle@gmail.com>
2018-04-08 21:23:40 +0200

    Fixed particles

--- SkWaze/src/fr/weefle/waze/nms/Particles.java
+++ SkWaze/src/fr/weefle/waze/nms/Particles.java
@@ -1,8 +1,8 @@
 package fr.weefle.waze.nms;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import fr.weefle.waze.Reflection;
 
@@ -10,12 +10,13 @@ public class Particles {
 	
 	Reflection reflection = new Reflection();
 	
-    public void sendParticles(Player player, String particles, boolean visible, float x, float y, float z, float xoff, float yoff, float zoff, float data, int number, int...is) throws ClassNotFoundException, SecurityException, NoSuchMethodException, IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchFieldException {
-    	Class<?> PacketPlayOutWorldParticles = reflection.getNMSClass("PacketPlayOutWorldParticles");
+    public void sendParticles(Player player, String particles,Location location, boolean visible, float xoff, float yoff, float zoff, float data, int number, int...is) throws ClassNotFoundException, SecurityException, NoSuchMethodException, IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchFieldException {
+    	/*Class<?> PacketPlayOutWorldParticles = reflection.getNMSClass("PacketPlayOutWorldParticles");
     	Class<?> EnumParticle = reflection.getNMSClass("EnumParticle");
         Constructor<?> packetConstructor = PacketPlayOutWorldParticles.getConstructor(EnumParticle, boolean.class, float.class, float.class, float.class, float.class, float.class, float.class, float.class, int.class, int[].class);
-        Object packet = packetConstructor.newInstance(EnumParticle.getField(particles).get(null), visible, x, y, z, xoff, yoff, zoff, data, number, is);
+        Object packet = packetConstructor.newInstance(EnumParticle.getField(particles).get(null), visible, (float) location.getBlockX(), (float) location.getBlockY(), (float) location.getBlockZ(), xoff, yoff, zoff, data, number, is);
         Method sendPacket = reflection.getConnection ( player ).getClass().getMethod ( "sendPacket", reflection.getNMSClass ( "Packet" ));
-        sendPacket.invoke (reflection.getConnection ( player ), packet );
+        sendPacket.invoke (reflection.getConnection ( player ), packet );*/
+    	player.getWorld().spawnParticle(Particle.valueOf(particles), location, number, xoff, yoff, zoff, data);
     }
     }
