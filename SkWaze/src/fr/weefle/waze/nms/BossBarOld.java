1f517a697ae673c1f8a2015ff9e48ea82e197df1
Weefle <mr.weefle@gmail.com>
2018-05-01 10:00:06 +0200

    Fixed 1.8 bossbar

--- SkWaze/src/fr/weefle/waze/nms/BossBarOld.java
+++ SkWaze/src/fr/weefle/waze/nms/BossBarOld.java
@@ -1,40 +1,28 @@
 package fr.weefle.waze.nms;
 
 import org.bukkit.entity.Player;
-import org.inventivetalent.bossbar.BossBarAPI.Color;
-import org.inventivetalent.bossbar.BossBarTimer;
-import org.inventivetalent.bossbar.PacketBossBar;
+import me.confuser.barapi.BarAPI;
 
 public class BossBarOld implements BossBarAPI {
-	
-	private PacketBossBar pb = null;
+
 	@Override
 	public void sendBossBar(Player p, String message, double percent, String color, String id) {
 		
-			//BarAPI.setMessage(p, message, (float) percent * 100);
-		pb.setMessage(message);
-        pb.setProgress((float) percent * 100);
-        pb.setColor(Color.valueOf(color));
-        pb.addPlayer(p);
+			BarAPI.setMessage(p, message, (float) percent * 100);
 			
 	}
 
 	@Override
 	public void removeBossBar(Player p, String id) {
 		
-			//BarAPI.removeBar(p);
-			pb.removePlayer(p);
+			BarAPI.removeBar(p);
 		
 	}
 
 	@Override
 	public void sendBossBarTimer(Player p, String message, double percent, String color, int time, String id) {
 		
-            //BarAPI.setMessage(p, message, time / 20);
-            new BossBarTimer(pb, (float) percent * 100, time / 20);
-            pb.setMessage(message);
-            pb.setColor(Color.valueOf(color));
-            pb.addPlayer(p);
+            BarAPI.setMessage(p, message, time / 20);
           
         }
 	
