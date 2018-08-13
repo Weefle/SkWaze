38e5790461ac3804f0752d757fb0429822cdaf40
Weefle <mr.weefle@gmail.com>
2018-08-13 23:01:25 +0200

    Fix

--- SkWaze/src/fr/weefle/waze/effects/WazeEffectNametag.java
+++ SkWaze/src/fr/weefle/waze/effects/WazeEffectNametag.java
@@ -19,7 +19,7 @@ public class WazeEffectNametag extends Effect {
 	public boolean init(Expression<?>[] arg0, int arg1, Kleenean arg2, ParseResult arg3) {
 		// TODO Auto-generated method stub
 		name = (Expression<String>) arg0[0];
-		player = (Expression<Player>) arg0[2];
+		player = (Expression<Player>) arg0[1];
 		return true;
 	}
 
