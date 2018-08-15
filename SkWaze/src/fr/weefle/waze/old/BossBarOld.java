11af04578ed495a716832e84b8a1f2e4a0ef60c7
Weefle <mr.weefle@gmail.com>
2018-08-15 19:18:43 +0200

    Fix

--- SkWaze/src/fr/weefle/waze/old/BossBarOld.java
+++ SkWaze/src/fr/weefle/waze/old/BossBarOld.java
@@ -2,34 +2,37 @@ package fr.weefle.waze.old;
 
 import org.bukkit.entity.Player;
 import fr.weefle.waze.nms.BossBarAPI;
-import me.confuser.barapi.BarAPI;
 
 public class BossBarOld implements BossBarAPI {
 
 	@Override
 	public void sendBossBar(Player p, String message, int percent, String color, String id) {
 		
-			BarAPI.setMessage(p, message, percent * 100);
+			//BarAPI.setMessage(p, message, percent);
+			org.inventivetalent.bossbar.BossBarAPI.setMessage(p, message, percent);
 			
 	}
 
 	@Override
 	public void removeBossBar(Player p, String id) {
 		
-			BarAPI.removeBar(p);
+			//BarAPI.removeBar(p);
+		org.inventivetalent.bossbar.BossBarAPI.removeBar(p);
 		
 	}
 
 	@Override
 	public void sendBossBarTimer(Player p, String message, int percent, String color, int time, String id) {
 		
-            BarAPI.setMessage(p, message, time / 20);
+            //BarAPI.setMessage(p, message, time);
+		org.inventivetalent.bossbar.BossBarAPI.setMessage(p, message, percent, time);
           
         }
 
 	@Override
 	public String getBossBar(Player p) {
-		return BarAPI.getMessage(p);
+		//return BarAPI.getMessage(p);
+		return org.inventivetalent.bossbar.BossBarAPI.getMessage(p);
 	}
 	
 }
