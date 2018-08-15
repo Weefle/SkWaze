ab7a8bcfc4dd040f801d2970581b71cee68e4ee0
Weefle <mr.weefle@gmail.com>
2018-08-15 13:41:40 +0200

    BarAPI is better

--- SkWaze/src/fr/weefle/waze/old/BossBarOld.java
+++ SkWaze/src/fr/weefle/waze/old/BossBarOld.java
@@ -2,38 +2,34 @@ package fr.weefle.waze.old;
 
 import org.bukkit.entity.Player;
 import fr.weefle.waze.nms.BossBarAPI;
-
+import me.confuser.barapi.BarAPI;
 
 public class BossBarOld implements BossBarAPI {
 
 	@Override
 	public void sendBossBar(Player p, String message, int percent, String color, String id) {
 		
-			//BarAPI.setMessage(p, message, percent * 100);
-			org.inventivetalent.bossbar.BossBarAPI.setMessage(p, message, percent);
+			BarAPI.setMessage(p, message, percent * 100);
 			
 	}
 
 	@Override
 	public void removeBossBar(Player p, String id) {
 		
-			//BarAPI.removeBar(p);
-		org.inventivetalent.bossbar.BossBarAPI.removeBar(p);
+			BarAPI.removeBar(p);
 		
 	}
 
 	@Override
 	public void sendBossBarTimer(Player p, String message, int percent, String color, int time, String id) {
 		
-            //BarAPI.setMessage(p, message, time / 20);
-		org.inventivetalent.bossbar.BossBarAPI.setMessage(p, message, percent, time);
+            BarAPI.setMessage(p, message, time / 20);
           
         }
 
 	@Override
 	public String getBossBar(Player p) {
-		//return BarAPI.getMessage(p);
-		return org.inventivetalent.bossbar.BossBarAPI.getMessage(p);
+		return BarAPI.getMessage(p);
 	}
 	
 }
