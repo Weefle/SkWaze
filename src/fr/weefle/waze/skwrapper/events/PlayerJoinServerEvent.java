63d84cf194162b590e5bb7b34b7869cd21ec4b78
Weefle <mr.weefle@gmail.com>
2021-03-18 00:01:33 +0100

    Fixes

--- src/fr/weefle/waze/skwrapper/events/PlayerJoinServerEvent.java
+++ src/fr/weefle/waze/skwrapper/events/PlayerJoinServerEvent.java
@@ -2,12 +2,14 @@ package fr.weefle.waze.skwrapper.events;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.event.*;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
-import java.util.StringTokenizer;
 
 public class PlayerJoinServerEvent extends Event implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
